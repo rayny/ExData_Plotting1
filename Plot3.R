e1815334eb893ea3dbe24d530d44c404c51d428c
Sergey Bronskiy <s.s.bronskiy@gmail.com>
2017-11-25 23:36:47 +0300

    Plot 3 added

---
+++ Plot3.R
@@ -0,0 +1,14 @@
+png(filename = "Plot3.png")
+data1 <- read.table("household_power_consumption.txt", as.is = TRUE, sep = ";", header = TRUE, na.strings = "?")
+data1 <- subset(data1, Date %in% c("1/2/2007", "2/2/2007"))
+data1$datetime <- strptime(paste(data1$Date, data1$Time), format = "%d/%m/%Y %H:%M:%S")
+
+plot(data1$datetime, data1$Sub_metering_1, type = "n", xlab = "", ylab = "Energy sub metering", xaxt="n")
+axis(1, at=seq(min(data1$datetime), max(data1$datetime), length.out = 3), labels = c("Thu", "Fri", "Sat"),col = NA, col.ticks = 1)
+
+lines(data1$datetime, data1$Sub_metering_1)
+lines(data1$datetime, data1$Sub_metering_2, col="red")
+lines(data1$datetime, data1$Sub_metering_3,col="purple")
+legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col = c("black", "red", "purple"), lty=1)
+
+dev.off()
